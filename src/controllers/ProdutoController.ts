5f29feb547d3dfe01dd55335775fc76c32e06106
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2023-01-05 17:25:38 -0300

    (ref)!: refatoração da rota e controller de produtos

--- src/controllers/ProdutoController.ts
+++ src/controllers/ProdutoController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { StatusProduto } from "../enums/StatusProduto";
 import { Produto } from "../models/Produto";
+import { validationResult } from "express-validator";
 
 const produtos: Produto[] = [];
 
@@ -9,7 +10,7 @@ produtos.push(new Produto(Math.random(),'Bacia','Uma bacia de 400ml perfeira par
 produtos.push(new Produto(Math.random(),'Copo','Um copo de 200ml perfeira para beber líquidos, como suco.', 2.50,['http...','http2...'],StatusProduto.Disponível));
 produtos.push(new Produto(Math.random(),'Garfo','Um garfo perfeira para comer, como macarrão.', 3.50,['http...','http2...'],StatusProduto.Disponível));
 
-export function pegarTodosStatusDisponivel(req: Request, res: Response): void {
+export function pegarTodosStatusDisponivel(_: Request, res: Response) {
     const prdRetorno = produtos.filter(prd => prd.status === StatusProduto.Disponível);
     res.status(200).send({
         status: "success",
@@ -19,8 +20,27 @@ export function pegarTodosStatusDisponivel(req: Request, res: Response): void {
     });
 }
 
-export function pegarPorId(req: Request, res: Response): void {
+export function pegarPorId(req: Request, res: Response) {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).send({
+            status: 'fail',
+            data:{
+                errors: errors.array().map(error => error.msg)
+            }
+        });
+    }
+    
     const prdRetorno = produtos.find(prd => prd.id === Number.parseFloat(req.params.id));
+    if(!prdRetorno){
+        return res.status(400).send({
+            status: 'fail',
+            data:{
+                message: "O 'id' do produto não foi encontrado."
+            }
+        });
+    }
+
     res.status(200).send({
         status: "success",
         data: {
@@ -29,7 +49,17 @@ export function pegarPorId(req: Request, res: Response): void {
     });
 }
 
-export function adicionar(req: Request,res: Response): void {
+export function adicionar(req: Request,res: Response) {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).send({
+            status: 'fail',
+            data:{
+                errors: errors.array().map(error => error.msg)
+            }
+        });
+    }
+
     const produto = req.body;
     produtos.push(
         new Produto(Math.random(),
@@ -49,7 +79,17 @@ export function adicionar(req: Request,res: Response): void {
     });
 }
 
-export function atualizaPorPartes(req: Request,res: Response): void {
+export function atualizaPorPartes(req: Request,res: Response){
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).send({
+            status: 'fail',
+            data:{
+                errors: errors.array().map(error => error.msg)
+            }
+        });
+    }
+    
     const indiceProduto = produtos.findIndex(produto => produto.id === Number.parseFloat(req.params.id));
     if(indiceProduto > -1){
         const produto = produtos[indiceProduto];
@@ -77,36 +117,62 @@ export function atualizaPorPartes(req: Request,res: Response): void {
     }
 }
 
-export function atualizaCompleto(req: Request,res: Response): void {
-    const indiceProduto = produtos.findIndex(produto => produto.id === Number.parseFloat(req.params.id));
-    if(indiceProduto > -1){
-        const produto = produtos[indiceProduto];
-
-        produto.nome = req.body.nome;
-        produto.descricao = req.body.descricao;
-        produto.preco = req.body.preco;
-        produto.imagens = req.body.imagens;
-        produto.status = req.body.status;
-
-        res.status(200).send();
-    }
-}
-
-export function deleta(req: Request,res: Response): void {
-    const indiceProduto = produtos.findIndex(produto => produto.id === Number.parseFloat(req.params.id));
-    if(indiceProduto > -1){
-        produtos.splice(indiceProduto, 1);
-        res.status(200).send({
-            status: "success",
-            data: null
-        });
-    }
-    else{
-        res.status(400).send({
-            status: "fail",
-            data: {
-                "message": "O id do produto não foi encontrado."
+export function atualizaCompleto(req: Request,res: Response){
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).send({
+            status: 'fail',
+            data:{
+                errors: errors.array().map(error => error.msg)
             }
         });
     }
+
+    const indiceProduto = produtos.findIndex(produto => produto.id === Number.parseFloat(req.params.id));
+    if(indiceProduto === -1){
+        return res.status(400).send({
+            status: "fail",
+            data: {
+                message: "O 'id' do produto não foi encontrado."
+            }
+        });
+    }
+
+    const produto = produtos[indiceProduto];
+
+    produto.nome = req.body.nome;
+    produto.descricao = req.body.descricao;
+    produto.preco = req.body.preco;
+    produto.imagens = req.body.imagens;
+    produto.status = req.body.status;
+
+    res.status(200).send();
+}
+
+export function deleta(req: Request,res: Response){
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).send({
+            status: 'fail',
+            data:{
+                errors: errors.array().map(error => error.msg)
+            }
+        });
+    }
+
+    const indiceProduto = produtos.findIndex(produto => produto.id === Number.parseFloat(req.params.id));
+    if(indiceProduto === -1){
+        return res.status(400).send({
+            status: "fail",
+            data: {
+                "message": "O 'id' do produto não foi encontrado."
+            }
+        });
+    }
+
+    produtos.splice(indiceProduto, 1);
+    res.status(200).send({
+        status: "success",
+        data: null
+    });
 }
--- src/routes/ProdutoRoutes.ts
+++ src/routes/ProdutoRoutes.ts
@@ -1,103 +1,43 @@
-import express, { Request, Response, Router } from "express";
+import express from "express";
 import { adicionar, atualizaCompleto, atualizaPorPartes, deleta, pegarPorId, pegarTodosStatusDisponivel } from "../controllers/ProdutoController";
-import { body, param, validationResult } from "express-validator";
+import { body, param } from "express-validator";
 
-export function ProdutoRoutes (baseUrl: string): Router {
-    const router = express.Router();
+const router = express.Router();
 
-    router.get(`${baseUrl}/produtos`, (req: Request, res: Response) => {
-        pegarTodosStatusDisponivel(req, res);
-    });
-    
-    router.get(`${baseUrl}/produtos/:id`, (req: Request, res: Response) => {
-        pegarPorId(req, res);
-    });
+router.get("/", pegarTodosStatusDisponivel);
+router.get("/:id",
+    param('id', "'id' inválido!").not().isEmpty().withMessage("'id' está vazio!").isNumeric({ no_symbols: false, locale: 'en-US'}).withMessage("'id' não possui um valor válido!"),
+    pegarPorId
+);
+router.post("/",
+    body('nome', "'nome' inválido!").not().isEmpty().withMessage("'nome' está vazio!").isString().withMessage("'nome' não possui um valor válido!"),
+    body('descricao', "'descricao' inválida!").not().isEmpty().withMessage("'descricao' está vazia!").isString().withMessage("'descricao' não possui um valor válido!"),
+    body('preco', "'preco' inválido!").not().isEmpty().withMessage("'preco' está vazio!").isNumeric({ no_symbols: false, locale: 'en-US'}).withMessage("'preco' não possui um valor válido!"),
+    body('imagens', "'imagens' inválido!").not().isEmpty().withMessage("'imagens' está vazio!").isArray().withMessage("'imagens' não possui um valor válido!"),
+    body('status', "'status' inválido!").not().isEmpty().withMessage("'status' está vazio!").not().isString().withMessage("'status' não possui o formato de dado correto!").isNumeric({ no_symbols: false, locale: 'en-US'}).withMessage("'status' não possui um valor válido!").isIn([0,1]).withMessage("'status' não possui o valor de um status possível!"),
+    adicionar
+);
+router.patch("/:id",
+    param('id', "'id' inválido!").not().isEmpty().withMessage("'id' está vazio!").isNumeric({ no_symbols: false, locale: 'en-US'}).withMessage("'id' não possui um valor válido!"),
+    body('nome', "'nome' inválido!").optional().not().isEmpty().withMessage("'nome' está vazio!").isString().withMessage("'nome' não possui um valor válido!"),
+    body('descricao', "'descricao' inválida!").optional().not().isEmpty().withMessage("'descricao' está vazia!").isString().withMessage("'descricao' não possui um valor válido!"),
+    body('preco', "'preco' inválido!").optional().not().isEmpty().withMessage("'preco' está vazio!").isNumeric({ no_symbols: false, locale: 'en-US'}).withMessage("'preco' não possui um valor válido!"),
+    body('imagens', "'imagens' inválido!").optional().not().isEmpty().withMessage("'imagens' está vazio!").isArray().withMessage("'imagens' não possui um valor válido!"),
+    body('status', "'status' inválido!").optional().not().isEmpty().withMessage("'status' está vazio!").not().isString().withMessage("'status' não possui o formato de dado correto!").isNumeric({ no_symbols: false, locale: 'en-US'}).withMessage("'status' não possui um valor válido!").isIn([0,1]).withMessage("'status' não possui o valor de um status possível!"),
+    atualizaPorPartes
+);
+router.put("/:id",
+    param('id').not().isEmpty().withMessage("'id' está vazio!").isNumeric({ no_symbols: false, locale: 'en-US'}).withMessage("'id' não possui um valor válido!"),
+    body('nome', "'nome' inválido!").not().isEmpty().withMessage("'nome' está vazio!").isString().withMessage("'nome' não possui um valor válido!"),
+    body('descricao', "'descricao' inválida!").not().isEmpty().withMessage("'descricao' está vazia!").isString().withMessage("'descricao' não possui um valor válido!"),
+    body('preco', "'preco' inválido!").not().isEmpty().withMessage("'preco' está vazio!").isNumeric({ no_symbols: false, locale: 'en-US'}).withMessage("'preco' não possui um valor válido!"),
+    body('imagens', "'imagens' inválido!").not().isEmpty().withMessage("'imagens' está vazio!").isArray().withMessage("'imagens' não possui um valor válido!"),
+    body('status', "'status' inválido!").not().isEmpty().withMessage("'status' está vazio!").not().isString().withMessage("'status' não possui o formato de dado correto!").isNumeric({ no_symbols: false, locale: 'en-US'}).withMessage("'status' não possui um valor válido!").isIn([0,1]).withMessage("'status' não possui o valor de um status possível!"),
+    atualizaCompleto
+);
+router.delete("/:id",
+    param('id').not().isEmpty().withMessage("'id' está vazio!").isNumeric({ no_symbols: false, locale: 'en-US'}).withMessage("'id' não possui um valor válido!"),
+    deleta
+);
 
-    router.post(`${baseUrl}/produtos`,
-        body('nome', "'nome' inválido!").not().isEmpty().withMessage("'nome' está vazio!").isString().withMessage("'nome' não possui um valor válido!"),
-        body('descricao', "'descricao' inválida!").not().isEmpty().withMessage("'descricao' está vazia!").isString().withMessage("'descricao' não possui um valor válido!"),
-        body('preco', "'preco' inválido!").not().isEmpty().withMessage("'preco' está vazio!").isNumeric({ no_symbols: false, locale: 'en-US'}).withMessage("'preco' não possui um valor válido!"),
-        body('imagens', "'imagens' inválido!").not().isEmpty().withMessage("'imagens' está vazio!").isArray().withMessage("'imagens' não possui um valor válido!"),
-        body('status', "'status' inválido!").not().isEmpty().withMessage("'status' está vazio!").not().isString().withMessage("'status' não possui o formato de dado correto!").isNumeric({ no_symbols: false, locale: 'en-US'}).withMessage("'status' não possui um valor válido!").isIn([0,1]).withMessage("'status' não possui o valor de um status possível!"),
-        (req: Request, res: Response) => {
-            const errors = validationResult(req);
-            if(!errors.isEmpty()){
-                res.status(400).send({
-                    status: 'fail',
-                    data:{
-                        errors: errors.array().map(error => error.msg)
-                    }
-                });
-            }
-            else{
-                adicionar(req, res);
-            }
-        }
-    );
-
-    router.patch(`${baseUrl}/produtos/:id`,
-        param('id', "'id' inválido!").not().isEmpty().withMessage("'id' está vazio!").isNumeric({ no_symbols: false, locale: 'en-US'}).withMessage("'id' não possui um valor válido!"),
-        body('nome', "'nome' inválido!").optional().not().isEmpty().withMessage("'nome' está vazio!").isString().withMessage("'nome' não possui um valor válido!"),
-        body('descricao', "'descricao' inválida!").optional().not().isEmpty().withMessage("'descricao' está vazia!").isString().withMessage("'descricao' não possui um valor válido!"),
-        body('preco', "'preco' inválido!").optional().not().isEmpty().withMessage("'preco' está vazio!").isNumeric({ no_symbols: false, locale: 'en-US'}).withMessage("'preco' não possui um valor válido!"),
-        body('imagens', "'imagens' inválido!").optional().not().isEmpty().withMessage("'imagens' está vazio!").isArray().withMessage("'imagens' não possui um valor válido!"),
-        body('status', "'status' inválido!").optional().not().isEmpty().withMessage("'status' está vazio!").not().isString().withMessage("'status' não possui o formato de dado correto!").isNumeric({ no_symbols: false, locale: 'en-US'}).withMessage("'status' não possui um valor válido!").isIn([0,1]).withMessage("'status' não possui o valor de um status possível!"),
-        (req: Request, res: Response) => {
-            const errors = validationResult(req);
-            if(!errors.isEmpty()){
-                res.status(400).send({
-                    status: 'fail',
-                    data:{
-                        errors: errors.array().map(error => error.msg)
-                    }
-                });
-            }
-            else{
-                atualizaPorPartes(req, res);
-            }
-        }
-    );
-
-    router.put(`${baseUrl}/produtos/:id`,
-        param('id').not().isEmpty().withMessage("'id' está vazio!").isNumeric({ no_symbols: false, locale: 'en-US'}).withMessage("'id' não possui um valor válido!"),
-        body('nome', "'nome' inválido!").not().isEmpty().withMessage("'nome' está vazio!").isString().withMessage("'nome' não possui um valor válido!"),
-        body('descricao', "'descricao' inválida!").not().isEmpty().withMessage("'descricao' está vazia!").isString().withMessage("'descricao' não possui um valor válido!"),
-        body('preco', "'preco' inválido!").not().isEmpty().withMessage("'preco' está vazio!").isNumeric({ no_symbols: false, locale: 'en-US'}).withMessage("'preco' não possui um valor válido!"),
-        body('imagens', "'imagens' inválido!").not().isEmpty().withMessage("'imagens' está vazio!").isArray().withMessage("'imagens' não possui um valor válido!"),
-        body('status', "'status' inválido!").not().isEmpty().withMessage("'status' está vazio!").not().isString().withMessage("'status' não possui o formato de dado correto!").isNumeric({ no_symbols: false, locale: 'en-US'}).withMessage("'status' não possui um valor válido!").isIn([0,1]).withMessage("'status' não possui o valor de um status possível!"),
-        (req: Request, res: Response) => {
-            const errors = validationResult(req);
-            if(!errors.isEmpty()){
-                res.status(400).send({
-                    status: 'fail',
-                    data:{
-                        errors: errors.array().map(error => error.msg)
-                    }
-                });
-            }
-            else {
-                atualizaCompleto(req, res);
-            }
-        }
-    );
-
-    router.delete(`${baseUrl}/produtos/:id`,
-        param('id').not().isEmpty().withMessage("'id' está vazio!").isNumeric({ no_symbols: false, locale: 'en-US'}).withMessage("'id' não possui um valor válido!"),
-        (req: Request, res: Response) => {
-            const errors = validationResult(req);
-            if(!errors.isEmpty()){
-                res.status(400).send({
-                    status: 'fail',
-                    data:{
-                        errors: errors.array().map(error => error.msg)
-                    }
-                });
-            }
-            else{
-                deleta(req, res);
-            }
-        }
-    );
-
-    return router;
-}
+export { router };
