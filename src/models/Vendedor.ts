ae8f7505d226d55fecb971cb6a482d755207f28f
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2023-01-06 22:27:49 -0300

    (feat): inclusão dos getters e setters nas classes e pequenas alterações em propriedades que existiam

--- src/models/Vendedor.ts
+++ src/models/Vendedor.ts
@@ -2,9 +2,17 @@ import { Usuario } from "./Usuario";
 import { Produto } from "./Produto";
 
 export class Vendedor extends Usuario {
-    private produtos?: Produto[];
+    private _produtos?: Produto[];
 
-    constructor(id: number, nome: string, email: string, senha: string) {
-        super(id,nome,email,senha);
+    constructor(uid: string, nome: string, email: string, senha: string) {
+        super(uid,nome,email,senha);
+    }
+
+    get produtos(): Produto[] {
+        return this._produtos!;
+    }
+
+    set produtos(produtos: Produto[]){
+        this._produtos = produtos;
     }
 }
