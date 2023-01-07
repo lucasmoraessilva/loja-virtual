10626e960dae097ef3bbb13a07b78f5041512bdf
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2023-01-07 11:42:02 -0300

    (feat): adição da rota de usuário e ajuste nos nomes das variáveis para não causar conflito

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import { router } from "./routes/ProdutoRoutes";
+import { router as produtoRouter } from "./routes/ProdutoRoutes";
+import { router as usuarioRouter } from "./routes/UsuarioRoutes";
 
 const app = express();
 
 app.use(express.json());
-app.use('/api/v1/produtos', router);
+app.use('/api/v1/produtos', produtoRouter);
+app.use('/api/v1/usuarios', usuarioRouter);
 
 export { app };
