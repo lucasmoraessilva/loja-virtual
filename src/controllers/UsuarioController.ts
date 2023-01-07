2c3cf9acf0d61e49f79a806f5e7679cb9005ccc5
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2023-01-07 11:41:10 -0300

    (feat): criação do método de login da controller de usuário

--- src/controllers/UsuarioController.ts
+++ src/controllers/UsuarioController.ts
@@ -1,5 +1,33 @@
 import { Request, Response } from "express";
+import { validationResult } from "express-validator";
+import { UsuarioDatabase } from "../database/UsuarioDatabase";
 
-export function login(res: Request, res: Response){
-    // função de login
+export function login(req: Request, res: Response){
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).send({
+            status: 'fail',
+            data:{
+                errors: errors.array().map(error => error.msg)
+            }
+        });
+    }
+
+    UsuarioDatabase.getInstance().login(req.body.email, req.body.senha)
+        .then(data => {
+            res.status(200).send({
+                status: 'success',
+                data: {
+                    uid: data
+                }
+            });
+        })
+        .catch(error => {
+            res.status(400).send({
+                status: 'fail',
+                data: {
+                    error: error.message
+                }
+            });
+        });
 }
