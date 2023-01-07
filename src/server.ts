467a7d14bf012874446495db4b46e9f768cf4fc9
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2023-01-06 21:49:14 -0300

    (feat): configuração para o uso da biblioteca dotenv no projeto

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,9 @@
 import { app } from './app';
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: './config.env'
+});
 
 app.listen(3000,() => {
     console.log('loja-virtual-api iniciada...');
