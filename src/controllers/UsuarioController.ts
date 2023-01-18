d9e94132ef6704a497c12d81d71605e0ba58e5c4
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2023-01-17 22:25:24 -0300

    (feat)!: substituição de ProdutoDatabase por UserRepository

--- src/controllers/UsuarioController.ts
+++ src/controllers/UsuarioController.ts
@@ -1,33 +1,46 @@
 import { Request, Response } from "express";
 import { validationResult } from "express-validator";
-import { UsuarioDatabase } from "../database/UsuarioDatabase";
+import jwt from "jsonwebtoken";
+import { IUserRepository } from "../repositories/IUserRepository";
 
-export function login(req: Request, res: Response){
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).send({
-            status: 'fail',
-            data:{
-                errors: errors.array().map(error => error.msg)
-            }
-        });
+export default class UsuarioController{
+    private userRepository: IUserRepository;
+
+    constructor(userRepository: IUserRepository) {
+        this.userRepository = userRepository;
     }
 
-    UsuarioDatabase.getInstance().login(req.body.email, req.body.senha)
-        .then(data => {
+    async login(req: Request, res: Response){
+        try {
+            const errors = validationResult(req);
+            if(!errors.isEmpty()){
+                return res.status(400).send({
+                    status: 'fail',
+                    data:{
+                        errors: errors.array().map(error => error.msg)
+                    }
+                });
+            }
+        
+            const _uid = await this.userRepository.login(req.body.email, req.body.senha);
+
+            const token = await jwt.sign({ _uid }, process.env.JWT_SECRET!, { expiresIn: "6h" });
+
             res.status(200).send({
                 status: 'success',
                 data: {
-                    uid: data
+                    token
                 }
             });
-        })
-        .catch(error => {
+        }
+        catch (error) {
             res.status(400).send({
                 status: 'fail',
                 data: {
                     error: error.message
                 }
             });
-        });
-}
+        }
+    }
+
+} 
