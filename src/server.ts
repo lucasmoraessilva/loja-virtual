5fdfa778c7c376b1408277d0d2df519f9c87b261
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2023-01-17 22:31:58 -0300

    (ref): ajuste no arquivo server.ts para execução correta da API

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import { app } from './app';
 import dotenv from "dotenv";
-
 dotenv.config({
-    path: './config.env'
+    path: `${__dirname}/../.env`
 });
+
+import { app } from './app';
 
 app.listen(3000,() => {
     console.log('loja-virtual-api iniciada...');
