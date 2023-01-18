849e650baf0d06b071460a89855699617180aa73
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2023-01-17 21:35:00 -0300

    (ref)!: substituição do ProdutoDatabase pelo MongoProductRepository

--- src/controllers/ProdutoController.ts
+++ src/controllers/ProdutoController.ts
@@ -1,169 +1,184 @@
 import { Request, Response } from "express";
 import { Produto } from "../models/Produto";
 import { validationResult } from "express-validator";
-import { ProdutoDatabase } from "../database/ProdutoDatabase";
 import { Types } from "mongoose";
+import { IProductRepository } from "../repositories/IProductRepository";
 
-export function pegarTodosStatusDisponivel(_: Request, res: Response) {
-    ProdutoDatabase.getInstance().pegarProdutosStatusDisponivel()
-        .then(data => {
+
+export default class ProdutoController {
+    private productRepository: IProductRepository;
+
+    constructor(productRepository: IProductRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public async pegarTodosStatusDisponivel(_: Request, res: Response) {
+        try {
+            const products = await this.productRepository.findAllWithAvailableStatus();
+    
             res.status(200).send({
                 status: "success",
-                data: {
-                    produtos: data
-                }
+                    data: {
+                        produtos: products
+                    }
             });
-        })
-        .catch(_ => {
+        }
+        catch (error) {
             res.status(500).send({
                 status: "fail",
                 data: {
                     message: "Não foi possível processar a sua requisição."
                 }
             });
-        })
-}
-
-export function pegarPorId(req: Request, res: Response) {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).send({
-            status: 'fail',
-            data:{
-                errors: errors.array().map(error => error.msg)
-            }
-        });
+        }
     }
     
-    ProdutoDatabase.getInstance().pegarProdutoPorUid(req.params.uid)
-        .then(data => {
+    async pegarPorId(req: Request, res: Response) {
+        try {
+            const errors = validationResult(req);
+            if(!errors.isEmpty()){
+                return res.status(400).send({
+                    status: 'fail',
+                    data:{
+                        errors: errors.array().map(error => error.msg)
+                    }
+                });
+            }
+
+            const product = await this.productRepository.findByUid(req.params.uid);
+    
             res.status(200).send({
                 status: "success",
                 data: {
-                    produto: data
+                    produto: product
                 }
             });
-        })
-        .catch(error => {
+        }
+        catch (error) {
             res.status(400).send({
                 status: 'fail',
                 data:{
                     error: error.message
                 }
             });
-        });   
-}
-
-export function adicionar(req: Request,res: Response) {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).send({
-            status: 'fail',
-            data:{
-                errors: errors.array().map(error => error.msg)
-            }
-        });
-    }
-
-    ProdutoDatabase.getInstance().adicionarProduto(
-        new Produto(
-            new Types.ObjectId().toHexString(),
-            req.body.nome,
-            req.body.descricao,
-            req.body.preco,
-            req.body.imagens,
-            req.body.status
-        )
-    )
-    .then(data => {
-        res.status(201).send({
-            status: "success",
-            data: {
-                produto: data
-            }
-        });
-    })
-    .catch(error =>{
-        res.status(400).send({
-            status: 'fail',
-            data:{
-                message: error.message
-            }
-        })
-    });
-}
-
-export function atualizaPorPartes(req: Request,res: Response){
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).send({
-            status: 'fail',
-            data:{
-                errors: errors.array().map(error => error.msg)
-            }
-        });
+        }
     }
     
-    ProdutoDatabase.getInstance().atualizarProduto(req.params.uid, req.body)
-    .then(_ => {
-        res.status(204).send();
-    })
-    .catch(error => {
-        res.status(400).send({
-            status: "fail",
-            data: {
-                error: error.message
+    async adicionar(req: Request,res: Response) {
+        try {
+            const errors = validationResult(req);
+            if(!errors.isEmpty()){
+                return res.status(400).send({
+                    status: 'fail',
+                    data:{
+                        errors: errors.array().map(error => error.msg)
+                    }
+                });
             }
-        });
-    });
-}
-
-export function atualizaCompleto(req: Request,res: Response){
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).send({
-            status: 'fail',
-            data:{
-                errors: errors.array().map(error => error.msg)
-            }
-        });
+        
+            const addedProduct = await this.productRepository.add(
+                new Produto(
+                    new Types.ObjectId().toHexString(),
+                    req.body.nome,
+                    req.body.descricao,
+                    req.body.preco,
+                    req.body.imagens,
+                    req.body.status
+                )
+            )
+            
+            res.status(201).send({
+                status: "success",
+                data: {
+                    produto: addedProduct
+                }
+            });
+        }
+        catch (error) {
+            res.status(400).send({
+                status: 'fail',
+                data:{
+                    message: error.message
+                }
+            })
+        }
     }
-
-    ProdutoDatabase.getInstance().atualizarProduto(req.params.uid, req.body)
-    .then(_ => {
-        res.status(204).send();
-    })
-    .catch(error => {
-        res.status(400).send({
-            status: "fail",
-            data: {
-                error: error.message
+    
+    async atualizaPorPartes(req: Request,res: Response){
+        try {
+            const errors = validationResult(req);
+            if(!errors.isEmpty()){
+                return res.status(400).send({
+                    status: 'fail',
+                    data:{
+                        errors: errors.array().map(error => error.msg)
+                    }
+                });
             }
-        });
-    });
-}
+            
+            await this.productRepository.patch(req.params.uid, req.body);
 
-export function deleta(req: Request,res: Response){
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).send({
-            status: 'fail',
-            data:{
-                errors: errors.array().map(error => error.msg)
-            }
-        });
+            res.status(204).send();
+        }
+        catch (error) {
+            res.status(400).send({
+                status: "fail",
+                data: {
+                    error: error.message
+                }
+            });
+        }
     }
-
-    ProdutoDatabase.getInstance().deletarProduto(req.params.uid)
-    .then(_ => {
-        res.status(204).send();
-    })
-    .catch(error => {
-        res.status(400).send({
-            status: "fail",
-            data: {
-                message: error.message
+    
+    async atualizaCompleto(req: Request,res: Response){
+        try {
+            const errors = validationResult(req);
+            if(!errors.isEmpty()){
+                return res.status(400).send({
+                    status: 'fail',
+                    data:{
+                        errors: errors.array().map(error => error.msg)
+                    }
+                });
             }
-        });
-    });
+        
+            await this.productRepository.update(req.params.uid, req.body);
+
+            res.status(204).send();
+        }
+        catch (error) {
+            res.status(400).send({
+                status: "fail",
+                data: {
+                    error: error.message
+                }
+            });
+        }
+    }
+    
+    async deleta(req: Request,res: Response){
+        try {
+            const errors = validationResult(req);
+            if(!errors.isEmpty()){
+                return res.status(400).send({
+                    status: 'fail',
+                    data:{
+                        errors: errors.array().map(error => error.msg)
+                    }
+                });
+            }
+        
+            this.productRepository.delete(req.params.uid);
+
+            res.status(204).send();
+        }
+        catch (error) {
+            res.status(400).send({
+                status: "fail",
+                data: {
+                    message: error.message
+                }
+            });   
+        }
+    }
 }
