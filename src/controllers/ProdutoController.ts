d6ef1f5313b7b35bd8256d779ea2674844ca3d07
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2023-01-20 07:36:35 -0300

    (ref)!: ajustes nas controllers para usar os middlewares de erro

--- src/controllers/ProdutoController.ts
+++ src/controllers/ProdutoController.ts
@@ -1,184 +1,100 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { Produto } from "../models/Produto";
 import { validationResult } from "express-validator";
 import { Types } from "mongoose";
 import { IProductRepository } from "../repositories/IProductRepository";
+import ValidationError from "../errors/ValidationError";
 
 
 export default class ProdutoController {
-    private productRepository: IProductRepository;
+    private static productRepository: IProductRepository;
 
     constructor(productRepository: IProductRepository) {
-        this.productRepository = productRepository;
+        ProdutoController.productRepository = productRepository;
     }
 
     public async pegarTodosStatusDisponivel(_: Request, res: Response) {
-        try {
-            const products = await this.productRepository.findAllWithAvailableStatus();
-    
-            res.status(200).send({
-                status: "success",
-                    data: {
-                        produtos: products
-                    }
-            });
-        }
-        catch (error) {
-            res.status(500).send({
-                status: "fail",
-                data: {
-                    message: "Não foi possível processar a sua requisição."
-                }
-            });
-        }
-    }
-    
-    async pegarPorId(req: Request, res: Response) {
-        try {
-            const errors = validationResult(req);
-            if(!errors.isEmpty()){
-                return res.status(400).send({
-                    status: 'fail',
-                    data:{
-                        errors: errors.array().map(error => error.msg)
-                    }
-                });
-            }
+        const products = await ProdutoController.productRepository.findAllWithAvailableStatus();
 
-            const product = await this.productRepository.findByUid(req.params.uid);
-    
-            res.status(200).send({
-                status: "success",
-                data: {
-                    produto: product
-                }
-            });
-        }
-        catch (error) {
-            res.status(400).send({
-                status: 'fail',
-                data:{
-                    error: error.message
-                }
-            });
-        }
+        res.status(200).send({
+            status: "success",
+            data: {
+                produtos: products
+            }
+        });
     }
     
-    async adicionar(req: Request,res: Response) {
-        try {
-            const errors = validationResult(req);
-            if(!errors.isEmpty()){
-                return res.status(400).send({
-                    status: 'fail',
-                    data:{
-                        errors: errors.array().map(error => error.msg)
-                    }
-                });
+    async pegarPorId(req: Request, res: Response, next: NextFunction) {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return next(new ValidationError(400, 'fail', errors.array().map(error => error.msg)));
+        }
+
+        const product = await ProdutoController.productRepository.findByUid(req.params.uid);
+
+        res.status(200).send({
+            status: "success",
+            data: {
+                produto: product
             }
-        
-            const addedProduct = await this.productRepository.add(
-                new Produto(
-                    new Types.ObjectId().toHexString(),
-                    req.body.nome,
-                    req.body.descricao,
-                    req.body.preco,
-                    req.body.imagens,
-                    req.body.status
-                )
+        });
+    }
+    
+    async adicionar(req: Request,res: Response, next: NextFunction) {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return next(new ValidationError(400, 'fail', errors.array().map(error => error.msg)));
+        }
+    
+        const addedProduct = await ProdutoController.productRepository.add(
+            new Produto(
+                new Types.ObjectId().toHexString(),
+                req.body.nome,
+                req.body.descricao,
+                req.body.preco,
+                req.body.imagens,
+                req.body.status
             )
-            
-            res.status(201).send({
-                status: "success",
-                data: {
-                    produto: addedProduct
-                }
-            });
-        }
-        catch (error) {
-            res.status(400).send({
-                status: 'fail',
-                data:{
-                    message: error.message
-                }
-            })
-        }
-    }
-    
-    async atualizaPorPartes(req: Request,res: Response){
-        try {
-            const errors = validationResult(req);
-            if(!errors.isEmpty()){
-                return res.status(400).send({
-                    status: 'fail',
-                    data:{
-                        errors: errors.array().map(error => error.msg)
-                    }
-                });
-            }
-            
-            await this.productRepository.patch(req.params.uid, req.body);
-
-            res.status(204).send();
-        }
-        catch (error) {
-            res.status(400).send({
-                status: "fail",
-                data: {
-                    error: error.message
-                }
-            });
-        }
-    }
-    
-    async atualizaCompleto(req: Request,res: Response){
-        try {
-            const errors = validationResult(req);
-            if(!errors.isEmpty()){
-                return res.status(400).send({
-                    status: 'fail',
-                    data:{
-                        errors: errors.array().map(error => error.msg)
-                    }
-                });
-            }
+        )
         
-            await this.productRepository.update(req.params.uid, req.body);
-
-            res.status(204).send();
-        }
-        catch (error) {
-            res.status(400).send({
-                status: "fail",
-                data: {
-                    error: error.message
-                }
-            });
-        }
+        res.status(201).send({
+            status: "success",
+            data: {
+                produto: addedProduct
+            }
+        });
     }
     
-    async deleta(req: Request,res: Response){
-        try {
-            const errors = validationResult(req);
-            if(!errors.isEmpty()){
-                return res.status(400).send({
-                    status: 'fail',
-                    data:{
-                        errors: errors.array().map(error => error.msg)
-                    }
-                });
-            }
+    async atualizaPorPartes(req: Request,res: Response, next: NextFunction){
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return next(new ValidationError(400, 'fail', errors.array().map(error => error.msg)));
+        }
         
-            this.productRepository.delete(req.params.uid);
+        await ProdutoController.productRepository.patch(req.params.uid, req.body);
 
-            res.status(204).send();
+        res.status(204).send();
+    }
+    
+    async atualizaCompleto(req: Request,res: Response, next: NextFunction){
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return next(new ValidationError(400, 'fail', errors.array().map(error => error.msg)));
         }
-        catch (error) {
-            res.status(400).send({
-                status: "fail",
-                data: {
-                    message: error.message
-                }
-            });   
+    
+        await ProdutoController.productRepository.update(req.params.uid, req.body);
+
+        res.status(204).send();
+    }
+    
+    async deleta(req: Request,res: Response, next: NextFunction){
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return next(new ValidationError(400, 'fail', errors.array().map(error => error.msg)));
         }
+    
+        ProdutoController.productRepository.delete(req.params.uid);
+
+        res.status(204).send();
     }
 }
--- src/controllers/UsuarioController.ts
+++ src/controllers/UsuarioController.ts
@@ -1,46 +1,31 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { validationResult } from "express-validator";
 import jwt from "jsonwebtoken";
+import ValidationError from "../errors/ValidationError";
 import { IUserRepository } from "../repositories/IUserRepository";
 
 export default class UsuarioController{
-    private userRepository: IUserRepository;
+    private static userRepository: IUserRepository;
 
     constructor(userRepository: IUserRepository) {
-        this.userRepository = userRepository;
+        UsuarioController.userRepository = userRepository;
     }
 
-    async login(req: Request, res: Response){
-        try {
-            const errors = validationResult(req);
-            if(!errors.isEmpty()){
-                return res.status(400).send({
-                    status: 'fail',
-                    data:{
-                        errors: errors.array().map(error => error.msg)
-                    }
-                });
+    async login(req: Request, res: Response, next: NextFunction){
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return next(new ValidationError(400, 'fail', errors.array().map(error => error.msg)));
+        }
+    
+        const _uid = await UsuarioController.userRepository.login(req.body.email, req.body.senha);
+
+        const token = await jwt.sign({ _uid }, process.env.JWT_SECRET!, { expiresIn: "6h" });
+
+        res.status(200).send({
+            status: 'success',
+            data: {
+                token
             }
-        
-            const _uid = await this.userRepository.login(req.body.email, req.body.senha);
-
-            const token = await jwt.sign({ _uid }, process.env.JWT_SECRET!, { expiresIn: "6h" });
-
-            res.status(200).send({
-                status: 'success',
-                data: {
-                    token
-                }
-            });
-        }
-        catch (error) {
-            res.status(400).send({
-                status: 'fail',
-                data: {
-                    error: error.message
-                }
-            });
-        }
+        });
     }
-
 } 
