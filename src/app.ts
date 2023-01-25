652668369bec13e5abeaf9d385cd1d1f9a4dac64
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2023-01-25 18:15:40 -0300

    (ref): atualização de nomes de variáveis e imports

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { router as productRouter } from "./routes/ProductRoutes";
-import { router as usuarioRouter } from "./routes/UsuarioRoutes";
+import { router as userRouter } from "./routes/UserRoutes";
 import { router as errorRouter } from "./routes/ErrorRoutes";
 import { errorHandlingMiddleware, validationErrorMiddleware } from "./controllers/ErrorController";
 
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use('/api/v1/seller', productRouter);
-app.use('/api/v1/usuarios', usuarioRouter);
+app.use('/api/v1/user', userRouter);
 app.all('*', errorRouter);
 app.use(validationErrorMiddleware);
 app.use(errorHandlingMiddleware);
