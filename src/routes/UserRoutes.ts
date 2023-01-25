aa8d913006c2c0e8f12276b8353ef29977df7e89
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2023-01-25 18:13:04 -0300

    (ref): atualização do import e nome das variáveis

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,16 +1,16 @@
 import { Router } from "express";
 import { body } from "express-validator";
-import UsuarioController from "../controllers/UsuarioController";
+import UserController from "../controllers/UserController";
 import MongoUserRepository from "../repositories/implementations/MongoUserRepository";
 import AsyncMiddlewareAdapter from "../adapters/AsyncMiddlewareAdapter";
 
 const router = Router();
-const usuarioController = new UsuarioController(new MongoUserRepository());
+const userController = new UserController(new MongoUserRepository());
 
 router.post("/login", 
     body('email', "'email' inválido!").not().isEmpty().withMessage("'email' está vazio!").isString().withMessage("'email' não possui um valor válido!").isEmail().withMessage("Formato do 'email' não é válido!"),
-    body('senha', "'senha' inválida").not().isEmpty().withMessage("'senha' está vazia!").isString().withMessage("'senha' não possui um valor válido!"),
-    AsyncMiddlewareAdapter(usuarioController.login)
+    body('password', "'password' inválida").not().isEmpty().withMessage("'password' está vazia!").isString().withMessage("'password' não possui um valor válido!"),
+    AsyncMiddlewareAdapter(userController.login)
 );
 
 export { router };
