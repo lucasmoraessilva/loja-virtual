5238adaa277407c564949707e98d76bba81ce80f
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2023-01-25 17:59:19 -0300

    (ref): definição do novo tipo de retorno da função login

--- src/repositories/IUserRepository.ts
+++ src/repositories/IUserRepository.ts
@@ -1,3 +1,3 @@
 export interface IUserRepository {
-    login(email: string, password: string): Promise<string>;
+    login(email: string, password: string): Promise<{ _uid: string, _type: string }>;
 }
