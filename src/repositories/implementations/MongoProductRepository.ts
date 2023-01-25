873cc159180365f6b5693e054be798895b96a93e
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2023-01-25 12:12:41 -0300

    (ref)!: adaptação para operações com o novo modelo de dados

--- src/repositories/implementations/MongoProductRepository.ts
+++ src/repositories/implementations/MongoProductRepository.ts
@@ -1,128 +1,198 @@
 import mongoose, { Schema, Model } from "mongoose";
-import { Produto } from "../../models/Produto";
-import { ProdutoSchema } from "../../schemas/ProdutoSchema";
+import { ProductStatus } from "../../enums/ProductStatus";
+import GenericApiError from "../../errors/GenericApiError";
+import { Product } from "../../models/Product";
+import { ProductSchema } from "../../schemas/ProductSchema";
+import { SellerSchema } from "../../schemas/SellerSchema";
 import { IProductRepository } from "../IProductRepository";
 
 export class MongoProductRepository implements IProductRepository {
     private connectionString: string;
-    private productSchema: Schema<typeof ProdutoSchema>;
-    private productModel: Model<typeof ProdutoSchema>;
+    private productSchema: Schema<typeof ProductSchema>;
+    private productModel: Model<typeof ProductSchema>;
+    private sellerSchema: Schema<typeof SellerSchema>;
+    private sellerModel: Model<typeof SellerSchema>;
 
     constructor() {
         this.connectionString = process.env.DB_CONNECTION_STRING!.replace('<PASSWORD>', process.env.DB_PASSWORD!);
-        this.productSchema = new Schema(ProdutoSchema);
-        this.productModel = mongoose.model('Product',this.productSchema, 'produtos');
+        this.productSchema = new Schema(ProductSchema);
+        this.productModel = mongoose.model('Product',this.productSchema, '');
+        this.sellerSchema = new Schema(SellerSchema);
+        this.sellerModel = mongoose.model('Seller',this.sellerSchema, 'sellers');
         mongoose.set('strictQuery', false);
     }
 
-    findAll(): Promise<Produto[]> {
+    private replaceProductValues(sourceProduct: any, productToBeUpdated: any){
+        Object.keys(sourceProduct).forEach(key => {
+            if(key !== 'sales'){
+                productToBeUpdated[`_${key}`] = sourceProduct[key];
+            }
+        });
+    }
+
+    findAll(sellerUid: string): Promise<Product[]> {
         return new Promise(async (resolve, reject) => {
             try {
                 mongoose.connect(this.connectionString);
-    
-                const prdModel = this.productModel;
 
-                const products = await prdModel.find({},'-_id _uid _nome _descricao _preco _imagens _status');
+                const seller = await this.sellerModel.findOne({ _uid: sellerUid }, '_products');
 
-                resolve(Object.assign(Array<Produto>(), products));
+                resolve(Object.assign(Array<Product>(), seller!._products));
             } catch (error) {
                 reject(error);
             }
         });
     }
 
-    findAllWithAvailableStatus(): Promise<Produto[]> {
+    findAllWithAvailableStatus(sellerUid: string): Promise<Product[]> {
         return new Promise(async (resolve, reject) => {
             try {
-                mongoose.connect(this.connectionString);
-    
-                const prdModel = this.productModel;
+                const products = await this.findAll(sellerUid);
 
-                const products = await prdModel.find({ _status: 1 }, '-_id _uid _nome _descricao _preco _imagens _status');
+                const availableProducts = products.filter(product => product._status === 1);
 
-                resolve(Object.assign(Array<Produto>(), products));
+                resolve(availableProducts);
             } catch (error) {
                 reject(error);
             }
         });
     }
 
-    findByUid(uid: string): Promise<Produto> {
+    findAvailableByUid(sellerUid: string, uid: string): Promise<Product> {
         return new Promise(async (resolve, reject) => {
             try {
-                mongoose.connect(this.connectionString);
+                const products = await this.findAllWithAvailableStatus(sellerUid);
     
-                const prdModel = this.productModel;
+                const product = products.find(product => product._uid === uid);
 
-                const product = await prdModel.findOne({ _uid: uid }, '-_id _uid _nome _descricao _preco _imagens _status');
+                if(!product){
+                    throw new GenericApiError('O produto não foi encontrado.', 400, 'fail');
+                }
                 
-                resolve(product!.toObject<Produto>());
+                resolve(product);
             } catch (error) {
                 reject(error);
             }
         });
     }
 
-    add(newProduct: Produto): Promise<Produto> {
+    findByUid(sellerUid: string, uid: string): Promise<Product> {
         return new Promise(async (resolve, reject) => {
             try {
-                mongoose.connect(this.connectionString);
+                const products = await this.findAll(sellerUid);
     
-                const prdModel = this.productModel;
+                const product = products.find(product => product._uid === uid);
 
-                const addedProduct = await prdModel.create(newProduct);
+                if(!product){
+                    throw new GenericApiError('O produto não foi encontrado.', 400, 'fail');
+                }
                 
-                resolve(addedProduct!.toObject<Produto>()); // Remover propriedades que não são importantes de estar na resposta
+                resolve(product);
             } catch (error) {
                 reject(error);
             }
         });
     }
 
-    update(uid: string, productToUpdate: Produto): Promise<Produto> {
+    add(sellerUid: string, newProduct: Product): Promise<Product> {
         return new Promise(async (resolve, reject) => {
             try {
                 mongoose.connect(this.connectionString);
-    
-                const prdModel = this.productModel;
 
-                await prdModel.replaceOne({ _uid: uid }, productToUpdate);
+                const newProductModel = new this.productModel({
+                    _uid: newProduct._uid,
+                    _name: newProduct._name,
+                    _description: newProduct._description,
+                    _price: newProduct._price,
+                    _images: newProduct._images,
+                    _status: newProduct._status
+                });
 
-                const updatedProduct = await prdModel.findOne({ _uid: uid });
+                const updateResult = await this.sellerModel.updateOne({ _uid: sellerUid }, { $push: { _products: newProductModel } });
 
-                resolve(updatedProduct!.toObject<Produto>()); // Remover propriedades que não são importantes de estar na resposta
+                if(!updateResult.acknowledged){
+                    throw new GenericApiError('Não foi possível adicionar o produto.', 400, 'fail');
+                }
+
+                const addedProduct = await this.findByUid(sellerUid, newProduct._uid);
+                
+                resolve(addedProduct);
             } catch (error) {
                 reject(error);
             }
         });
     }
 
-    patch(uid: string, productToUpdate: Produto): Promise<Produto> {
+    update(sellerUid: string, uid: string, productToUpdate: Product): Promise<Product> {
         return new Promise(async (resolve, reject) => {
             try {
                 mongoose.connect(this.connectionString);
-    
-                const prdModel = this.productModel;
 
-                await prdModel.updateOne({ _uid: uid }, productToUpdate);
+                const sales = (await this.findByUid(sellerUid, uid)).sales;
 
-                const updatedProduct = await prdModel.findOne({ _uid: uid });
+                const newProductModel = new this.productModel({
+                    _uid: uid,
+                    _name: productToUpdate.name,
+                    _description: productToUpdate.description,
+                    _price: productToUpdate.price,
+                    _images: productToUpdate.images,
+                    _status: productToUpdate.status,
+                    _sales: sales
+                });
 
-                resolve(updatedProduct!.toObject<Produto>()); // Remover propriedades que não são importantes de estar na resposta
+                const updateResult = await this.sellerModel.updateOne({ '_products._uid': uid }, { $set: { '_products.$':  newProductModel } });
+
+                if(!updateResult.modifiedCount){
+                    throw new GenericApiError('Não foi possível atualizar o produto.', 400, 'fail');
+                }
+
+                const updatedProduct = await this.findByUid(sellerUid, uid);
+
+                resolve(updatedProduct);
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
+
+    patch(sellerUid: string, uid: string, productToUpdate: Product): Promise<Product> {
+        return new Promise(async (resolve, reject) => {
+            try {
+                mongoose.connect(this.connectionString);
+
+                const productToBeUpdated = await this.findByUid(sellerUid, uid);
+
+                this.replaceProductValues(productToUpdate, productToBeUpdated);
+
+                const updateResult = await this.sellerModel.updateOne({ '_products._uid': uid }, { $set: { '_products.$':  productToBeUpdated } });
+
+                if(!updateResult.modifiedCount){
+                    throw new GenericApiError('Não foi possível atualizar o produto.', 400, 'fail');
+                }
+
+                const productUpdated = await this.findByUid(sellerUid, uid);
+
+                resolve(productUpdated);
             } catch (error) {
                 reject(error);
             }
         });
     }
     
-    delete(uid: string): Promise<void> {
+    delete(sellerUid: string, uid: string): Promise<void> {
         return new Promise(async (resolve, reject) => {
             try {
                 mongoose.connect(this.connectionString);
-    
-                const prdModel = this.productModel;
 
-                await prdModel.deleteOne({ _uid: uid });
+                const productToBeRemoved = await this.findByUid(sellerUid, uid);
+
+                productToBeRemoved._status = ProductStatus.Deleted.valueOf();
+
+                const updateResult = await this.sellerModel.updateOne({ '_products._uid': uid }, { $set: { '_products.$':  productToBeRemoved } });
+
+                if(!updateResult.modifiedCount){
+                    throw new GenericApiError('Não foi possível excluir o produto.', 400, 'fail');
+                }
                 
                 resolve();
             } catch (error) {
