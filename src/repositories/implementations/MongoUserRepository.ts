36a184d4183bea9c2a0ae57fe9a0cc2c6652da7c
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2023-01-25 18:04:30 -0300

    (feat): criação da função de login no nível de banco de dados

--- src/repositories/implementations/MongoUserRepository.ts
+++ src/repositories/implementations/MongoUserRepository.ts
@@ -1,34 +1,35 @@
-import mongoose, { Schema, Model } from "mongoose";
+import mongoose from "mongoose";
 import GenericApiError from "../../errors/GenericApiError";
-import { VendedorSchema } from "../../schemas/VendedorSchema";
+import { BuyerModel } from "../../models/BuyerModel";
+import { SellerModel } from "../../models/SellerModel";
 import { IUserRepository } from "../IUserRepository";
 
 export default class MongoUserRepository implements IUserRepository {
     private connectionString: string;
-    private userSchema: Schema<typeof VendedorSchema>;
-    private userModel: Model<typeof VendedorSchema>;
 
     constructor() {
         this.connectionString = process.env.DB_CONNECTION_STRING!.replace('<PASSWORD>', process.env.DB_PASSWORD!);
-        this.userSchema = new Schema(VendedorSchema);
-        this.userModel = mongoose.model('Vendedor',this.userSchema, 'usuarios');
         mongoose.set('strictQuery', false);
     }
-
-    login(email: string, password: string): Promise<string> {
+    
+    login(email: string, password: string): Promise<{ _uid: string, _type: string }> {
         return new Promise(async (resolve, reject) => {
             try {
                 mongoose.connect(this.connectionString);
                 
-                const existsResult = await this.userModel.exists({ _email: email, _senha: password });
+                let user = await SellerModel.findOne({ _email: email, _password: password });
 
-                if(!existsResult){
-                    throw new GenericApiError(`E-mail e/ou senha incorretos.`, 400, 'fail');
+                if(user){
+                    return resolve({ _uid: user._uid, _type: 'seller' });
                 }
 
-                const findResult = await this.userModel.findOne({ _id: existsResult._id }, '_uid -_id');
+                user = await BuyerModel.findOne({ _email: email, _password: password });
 
-                resolve(String(findResult!._uid));
+                if(user){
+                    return resolve({ _uid: user._uid, _type: 'buyer' });
+                }
+
+                throw new GenericApiError('E-mail ou senha incorreto.', 400, 'fail');
             }
             catch (error) {
                 reject(error)
