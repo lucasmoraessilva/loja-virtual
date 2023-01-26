e72c5358350459af8ff6eec576723f000b8c07cf
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2023-01-26 19:16:26 -0300

    (feat)!: adição do middleware de autorização nas rotas protegidas

--- src/routes/ProductRoutes.ts
+++ src/routes/ProductRoutes.ts
@@ -3,18 +3,21 @@ import ProductController from "../controllers/ProductController";
 import { body, param } from "express-validator";
 import { MongoProductRepository } from "../repositories/implementations/MongoProductRepository";
 import AsyncMiddlewareAdapter from "../adapters/AsyncMiddlewareAdapter";
+import { authorization } from "../controllers/SecurityController";
 
 const router = Router();
 const productController = new ProductController(new MongoProductRepository());
 
 router.get("/:sellerUid/products",
     param('sellerUid', "'sellerUid' inválido!").not().isEmpty().withMessage("'sellerUid' está vazio!").isString().withMessage("'sellerUid' não possui um valor válido!").isHexadecimal().withMessage("'sellerUid' não possui um formato válido!"),
+    AsyncMiddlewareAdapter(authorization('seller')),
     AsyncMiddlewareAdapter(productController.getAll)
 );
 
 router.get("/:sellerUid/products/:uid",
     param('sellerUid', "'sellerUid' inválido!").not().isEmpty().withMessage("'sellerUid' está vazio!").isString().withMessage("'sellerUid' não possui um valor válido!").isHexadecimal().withMessage("'sellerUid' não possui um formato válido!"),
     param('uid', "'uid' inválido!").not().isEmpty().withMessage("'uid' está vazio!").isString().withMessage("'uid' não possui um valor válido!").isHexadecimal().withMessage("'uid' não possui um formato válido!"),
+    AsyncMiddlewareAdapter(authorization('seller')),
     AsyncMiddlewareAdapter(productController.getByUid)
 );
 
@@ -29,13 +32,14 @@ router.get("/:sellerUid/available-products/:uid",
     AsyncMiddlewareAdapter(productController.getAvailableByUid)
 );
 
-router.post("/:sellerUid",
+router.post("/:sellerUid/products",
     param('sellerUid', "'sellerUid' inválido!").not().isEmpty().withMessage("'sellerUid' está vazio!").isString().withMessage("'sellerUid' não possui um valor válido!").isHexadecimal().withMessage("'sellerUid' não possui um formato válido!"),
     body('name', "'name' inválido!").not().isEmpty().withMessage("'name' está vazio!").isString().withMessage("'name' não possui um valor válido!"),
     body('description', "'description' inválida!").not().isEmpty().withMessage("'description' está vazia!").isString().withMessage("'description' não possui um valor válido!"),
     body('price', "'price' inválido!").not().isEmpty().withMessage("'price' está vazio!").isNumeric({ no_symbols: false, locale: 'en-US'}).withMessage("'price' não possui um valor válido!"),
     body('images', "'images' inválido!").not().isEmpty().withMessage("'images' está vazio!").isArray().withMessage("'images' não possui um valor válido!"),
     body('status', "'status' inválido!").not().isEmpty().withMessage("'status' está vazio!").not().isString().withMessage("'status' não possui o formato de dado correto!").isNumeric({ no_symbols: false, locale: 'en-US'}).withMessage("'status' não possui um valor válido!").isIn([0,1]).withMessage("'status' não possui o valor de um status possível!"),
+    AsyncMiddlewareAdapter(authorization('seller')),
     AsyncMiddlewareAdapter(productController.adicionar)
 );
 
@@ -47,6 +51,7 @@ router.patch("/:sellerUid/products/:uid",
     body('price', "'price' inválido!").optional().not().isEmpty().withMessage("'price' está vazio!").isNumeric({ no_symbols: false, locale: 'en-US'}).withMessage("'price' não possui um valor válido!"),
     body('images', "'images' inválido!").optional().not().isEmpty().withMessage("'images' está vazio!").isArray().withMessage("'images' não possui um valor válido!"),
     body('status', "'status' inválido!").optional().not().isEmpty().withMessage("'status' está vazio!").not().isString().withMessage("'status' não possui o formato de dado correto!").isNumeric({ no_symbols: false, locale: 'en-US'}).withMessage("'status' não possui um valor válido!").isIn([0,1]).withMessage("'status' não possui o valor de um status possível!"),
+    AsyncMiddlewareAdapter(authorization('seller')),
     AsyncMiddlewareAdapter(productController.atualizaPorPartes)
 );
 
@@ -58,12 +63,14 @@ router.put("/:sellerUid/products/:uid",
     body('price', "'price' inválido!").not().isEmpty().withMessage("'price' está vazio!").isNumeric({ no_symbols: false, locale: 'en-US'}).withMessage("'price' não possui um valor válido!"),
     body('images', "'images' inválido!").not().isEmpty().withMessage("'images' está vazio!").isArray().withMessage("'images' não possui um valor válido!"),
     body('status', "'status' inválido!").not().isEmpty().withMessage("'status' está vazio!").not().isString().withMessage("'status' não possui o formato de dado correto!").isNumeric({ no_symbols: false, locale: 'en-US'}).withMessage("'status' não possui um valor válido!").isIn([0,1]).withMessage("'status' não possui o valor de um status possível!"),
+    AsyncMiddlewareAdapter(authorization('seller')),
     AsyncMiddlewareAdapter(productController.atualizaCompleto)
 );
 
 router.delete("/:sellerUid/products/:uid",
     param('sellerUid', "'sellerUid' inválido!").not().isEmpty().withMessage("'sellerUid' está vazio!").isString().withMessage("'sellerUid' não possui um valor válido!").isHexadecimal().withMessage("'sellerUid' não possui um formato válido!"),
     param('uid', "'uid' inválido!").not().isEmpty().withMessage("'uid' está vazio!").isString().withMessage("'uid' não possui um valor válido!").isHexadecimal().withMessage("'uid' não possui um formato válido!"),
+    AsyncMiddlewareAdapter(authorization('seller')),
     AsyncMiddlewareAdapter(productController.deleta)
 );
 
